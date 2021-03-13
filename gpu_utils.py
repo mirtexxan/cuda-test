a0b54c143cda3e87f9398971ff4b2d94fcc309e9
mirtexxan <31573172+mirtexxan@users.noreply.github.com>
2021-03-13 20:09:04 +0100

    Matrix multiplication fixed and streamlined. Added a simple testing code, and a matlab example on how to use it.

--- gpu_utils.py
+++ gpu_utils.py
@@ -1,4 +1,8 @@
-from numba import cuda
+from numba import cuda, float32
+import time
+
+N_THREADS = 16
+
 
 def print_debug_info():
     gpu = cuda.get_current_device()
@@ -18,3 +22,77 @@ def print_debug_info():
     print("unifiedAddressing = %s" % str(gpu.UNIFIED_ADDRESSING))
     print("pciBusID = %s" % str(gpu.PCI_BUS_ID))
     print("pciDeviceID = %s" % str(gpu.PCI_DEVICE_ID))
+
+
+def timing(func):
+    def wrapper(*args):
+        if "kernel" in func.__name__:
+            start = time.time()
+            func(*args)
+            end = time.time()
+            print(f"Kernel took {end - start} seconds (including compilation)")
+            start = time.time()
+            func(*args)
+            end = time.time()
+            print(f"Kernel took {end - start} seconds (excluding compilation)")
+        else:
+            start = time.time()
+            func(*args)
+            end = time.time()
+            print(f"Host function took {end - start} seconds")
+    return wrapper
+
+
+@timing
+def run_numba_kernel(func, bpg, tpb, *args):
+    func[bpg, tpb](*args)
+
+
+# for testing purposes
+@cuda.jit('void(float32[:,:], float32[:,:], float32[:,:])')
+def naive_matmul(A, B, C):
+    i, j = cuda.grid(2)
+    if i < C.shape[0] and j < C.shape[1]:
+        tmp = 0.
+        for k in range(A.shape[1]):
+            tmp += A[i, k] * B[k, j]
+        C[i, j] = tmp
+
+
+# for testing purposes (NOTE: is very slow because it does not avoid useless computations)
+@cuda.jit('void(float32[:,:], float32[:,:], float32[:,:])')
+def fast_matmul(A, B, C):
+    # Define an array in the shared memory
+    # The size and type of the arrays must be known at compile time
+    sA = cuda.shared.array(shape=(N_THREADS, N_THREADS), dtype=float32)
+    sB = cuda.shared.array(shape=(N_THREADS, N_THREADS), dtype=float32)
+
+    x, y = cuda.grid(2)
+
+    # thread indexes
+    tx = cuda.threadIdx.x
+    ty = cuda.threadIdx.y
+    # blocks per grid
+    bpg_x = cuda.gridDim.x
+    bpg_y = cuda.gridDim.y
+
+    # Each thread computes one element in the result matrix.
+    # The dot product is chunked into dot products of N_THREADS-long vectors.
+    tmp = 0.
+    for i in range(max(bpg_x, bpg_y)):
+        # Preload data into shared memory
+        sA[tx, ty] = 0.
+        sB[tx, ty] = 0.
+        if x < A.shape[0] and (ty + i * N_THREADS) < A.shape[1]:
+            sA[tx, ty] = A[x, ty + i * N_THREADS]
+        if y < B.shape[1] and (tx + i * N_THREADS) < B.shape[0]:
+            sB[tx, ty] = B[tx + i * N_THREADS, y]
+        # Wait until all threads finish preloading
+        cuda.syncthreads()
+        # Computes partial product on the shared memory
+        for j in range(N_THREADS):
+            tmp += sA[tx, j] * sB[j, ty]
+        # Wait until all threads finish computing
+        cuda.syncthreads()
+    if x < C.shape[0] and y < C.shape[1]:
+        C[x, y] = tmp
--- run.py
+++ run.py
@@ -1,35 +1,78 @@
-import pandas as pd
+from numba import cuda
 import numpy as np
 import glob
 import os
 
-DEBUG = True
+from gpu_utils import run_numba_kernel, fast_matmul, naive_matmul
+from cuda_correlation import cross_corr, cross_corr_opt
+
+DEBUG = False
 
 if DEBUG:
-    PATH_PATTERN = "data/ts10*"
+    PATH_PATTERN = "data/ts10.np*"
 else:
-    PATH_PATTERN = "data/*"
+    PATH_PATTERN = "data/ts1*"
 
 
 def csv_to_npy(filename):
     print(f"Reading {filename}...")
-    array = np.loadtxt(filename, delimiter=',',)
+    array = np.loadtxt(filename, delimiter=',', )
     numpy_filename = os.path.splitext(filename)[0]
     np.save(numpy_filename, array)
+    return numpy_filename
 
 
-# TODO work in progress
+# TODO work in progress: here we should call correlation functions from cuda_correlation.py
 def process_array(array):
     pass
 
 
-if __name__ == "__main__":
-    for file in glob.glob(PATH_PATTERN):
+def test_matmult(matrix_a=None, matrix_b=None):
+    if not cuda.is_available():
+        print("Cuda is not available on this machine.")
+        exit()
+
+    if not matrix_a or not matrix_b:
+        rows = 63
+        cols = 42600
+        matrix_a = np.random.rand(rows, cols).astype('f')
+        matrix_b = np.random.rand(cols, rows).astype('f')
+        # matrix_a = np.ones((rows,cols), dtype=np.float32)
+        # matrix_b = np.ones((cols, rows), dtype=np.float32)
+    else:
+        matrix_a = np.array(matrix_a)
+        matrix_b = np.array(matrix_b)
+
+    matrix_c = np.empty((matrix_a.shape[0], matrix_b.shape[1]), dtype=np.float32)
+
+    args = (matrix_a, matrix_b, matrix_c)
+    n_threads = 16
+    tpb = (n_threads, n_threads)
+    n_blocks_x = (matrix_a.shape[0] + n_threads - 1) // n_threads
+    # n_blocks_y = (matrix_a.shape[1] + n_threads - 1) // n_threads
+    bpg = (n_blocks_x, n_blocks_x)
+    print(f"Running matrix multiplication with bpg={bpg} and tpb={tpb}")
+    run_numba_kernel(naive_matmul, bpg, tpb, *args)
+    return matrix_c
+
+def load_data(path):
+    list = []
+    for file in glob.glob(path):
         file_extension = os.path.splitext(file)[1]
         if file_extension == '.csv':
             csv_to_npy(file)
-        elif file_extension == '.npy':
+    for file in glob.glob(path):
+        file_extension = os.path.splitext(file)[1]
+        if file_extension == '.npy':
             array = np.load(file)
             print(f"Array {file} has shape {np.shape(array)}")
-            process_array(array)
+            list.append(array)
+    return list
 
+
+if __name__ == "__main__":
+    np.random.seed(240387)
+    #matrix_list = load_data(PATH_PATTERN)
+    # for matrix in matrix_list:
+    #    print(np.shape(matrix))
+    test_matmult()
