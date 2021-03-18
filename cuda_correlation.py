c85227dee70ce3ceaed4b8535fc5812b5ff38e43
mirtexxan <31573172+mirtexxan@users.noreply.github.com>
2021-03-18 04:03:29 +0100

    Implemented the "find maximum correlation function"

--- cuda_correlation.py
+++ cuda_correlation.py
@@ -1,103 +1,63 @@
 import numpy as np
-from numba import cuda
-import math
 import cupy as cp
 
-from gpu_utils import timing
+from utils import timing
 
 
-# TODO: implement maximum and fix how data is returned to the caller
-def cross_corr_opt(result_list, data_list, lag_list):
+def find_maximum_correlation(cp_matrix):
+    maximum = cp_matrix.max(axis=0)
+    minimum = cp_matrix.min(axis=0)
+    max_matrix = cp.where(cp.asarray(-minimum > maximum), minimum, maximum)
+    index_max_matrix = cp.abs(cp_matrix).argmax(axis=0) - cp_matrix.shape[0] // 2
+    return cp.stack((max_matrix, index_max_matrix), axis=0)
+
+
+def cross_correlation(result_list, data_list, lag_list):
     for i in range(len(data_list)):
+        # print(f"Working on filter {i+1} of {len(data_list)}")
+        tmp = cp.array(data_list[i])
         for lag in range(-lag_list[i], lag_list[i] + 1):
             # shifting a matrix using cp.pad (see numpy documentation)
-            if lag > 0:
-                data_shifted = cp.pad(data_list[i], ((0,0),(lag,0)), mode='constant')[:,:-lag]
+            if lag > 0 :
+                data_shifted = cp.pad(tmp, ((0, 0), (lag, 0)), mode='constant')[:, :-lag]
             elif lag < 0:
-                data_shifted = cp.pad(data_list[i], ((0, 0), (0, -lag)), mode='constant')[:, -lag:]
+                data_shifted = cp.pad(tmp, ((0, 0), (0, -lag)), mode='constant')[:, -lag:]
             else:
-                data_shifted = data_list[i]
-            corr_matrix = cp.matmul(data_list[i], cp.transpose(data_shifted)) / (data_list[i].shape[1]-abs(lag))
+                data_shifted = tmp
+            corr_matrix = cp.matmul(tmp, cp.transpose(data_shifted)) / (tmp.shape[1] - abs(lag))
             index = lag + lag_list[i]
             result_list[i][index] = corr_matrix
-        # compute the maximum correlation
-        print(result_list[i])
-        print("-----------------------")
-        print(np.maximum.reduce(cp.asnumpy(cp.abs(result_list[i]))))
-
-
-@cuda.jit('float32[:,:,:], float32[:,:], int32')
-def cuda_corr_naive(result, data_array, lag_value):
-    """
-    results : numpy array 3-D: Correlation array. Shape = (lag, neuron1, neuron2)
-    data_array : numpy array 2-D: Input data array. Shape = (neurons, timeseries)
-    lag_value : int: base lag for the given filter.
-    """
-    x, y = cuda.grid(2)
-    time_steps = data_array.shape[1]
-    for lag in range(-lag_value, lag_value+1):
-        correlation = 0.0
-        if x >= result.shape[1] or y >= result.shape[2]:
-            continue
-        if x >= y: # using the = sign here means don't computer auto-correlation
-            result[lag + lag_value, x, y] = 0.0
-            continue
-        for t in range(time_steps):
-            if t + lag < 0 or t + lag >= time_steps:
-                continue
-            else:
-                correlation += data_array[x, t] * data_array[y, t+lag]
-
-        correlation = correlation / (time_steps - abs(lag))
-        result[lag + lag_value, x, y] = correlation
-
-
-def cross_corr_naive(result_list, data_list, lag_list):
-    if len(data_list) != len(data_list) or  len(data_list) != len(result_list):
-        print("Mismatched data and lags input")
-        return
-
-    threads_per_block = (4, 8)
-    for i in range(len(data_list)):
-        bpg_x = int(math.ceil(data_list[i].shape[0] / threads_per_block[0]))
-        bpg_y = int(math.ceil(data_list[i].shape[1] / threads_per_block[1]))
-        blocks_per_grid = (bpg_x, bpg_y)
-        # print(f"Launching kernel {i+1} of {len(data_list)}")
-        cuda_corr_naive[blocks_per_grid, threads_per_block](result_list[i], data_list[i], lag_list[i])
+        result_list[i] = find_maximum_correlation(result_list[i])
 
 
 @timing
-def test(opt=True):
+def test():
     # example data size is setup based on the given examples
-    # WORST CASE SCENARIO: 174s with N_NEURONS = 63, TIMESTEP = 425.000, N_FILTERS = 10, TEST_LAG = 120 for each filter
-    # WORST CASE SCENARIO (OPT): 6s with N_NEURONS = 63, TIMESTEP = 425.000, N_FILTERS = 10, TEST_LAG = 120 for each filter
+    # TIMING ON RXT2080Ti: 6s with N_NEURONS = 63, TIMESTEP = 425.000, N_FILTERS = 10, LAG_LIST = 120 for each filter
     N_NEURONS = 3
-    TIMESTEPS = 10
-    N_FILTERS = 1
-    TEST_LAG = [2]*N_FILTERS
-    assert(N_FILTERS==len(TEST_LAG))
+    TIMESTEPS = 3
+    N_FILTERS = 2
+    LAG_LIST = [2] * N_FILTERS
+    assert (N_FILTERS == len(LAG_LIST))
 
     np.random.seed(240387)
     data_list = []
     result_list = []
     lag_list = []
     for i in range(N_FILTERS):
-        lag_list.append(TEST_LAG[i])
-        n_lags = 2 * TEST_LAG[i] + 1
-        data_list.append(cp.random.rand(N_NEURONS, TIMESTEPS).astype('f'))
+        lag_list.append(LAG_LIST[i])
+        n_lags = 2 * LAG_LIST[i] + 1
+        data_list.append(np.random.random_sample((N_NEURONS, TIMESTEPS)).astype('f') * 2 - 1)
         # data_list.append(cp.ones((N_NEURONS, TIMESTEPS), dtype=np.float32))
         result_list.append(cp.empty((n_lags, N_NEURONS, N_NEURONS), dtype=np.float32))
 
-    if opt:
-        cross_corr_opt(result_list, data_list, lag_list)
-        # print(result_list[0][:])
-    else:
-        cross_corr_naive(result_list, data_list, lag_list)
-        # print(result_list[0][:])
+    cross_correlation(result_list, data_list, lag_list)
     return result_list
+
+
+def check_symmetric(matrix, antisym=False, rtol=1e-05, atol=1e-08):
+    return np.allclose(matrix, matrix.T*(-1 if antisym else 1), rtol=rtol, atol=atol)
 
 
 if __name__ == "__main__":
     res = test()
-    # TODO: fix res is always None here
-    print(res)
