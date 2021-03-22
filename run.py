9868fc568c7bf51b33fbd9089cde809b97deaa8d
mirtexxan <31573172+mirtexxan@users.noreply.github.com>
2021-03-22 20:57:25 +0100

    Minor fixing and cleaning on run.py

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ import os
 import re
 import cupy as cp
 
-from utils import timing
 from cuda_correlation import cross_correlation
 
 
@@ -16,16 +15,7 @@ def csv_to_npy(filename):
     return numpy_filename
 
 
-@timing
-def do_correlation(m_list, l_list):
-    assert(len(m_list)==len(l_list))
-    r_list = [cp.empty((2*l_list[i]+1, m_list[i].shape[0], m_list[i].shape[0]), dtype=np.float32) for i in range(len(m_list))]
-    cross_correlation(r_list, m_list, l_list)
-    return r_list
-
-
 def test_matmult(matrix_a=None, matrix_b=None):
-
     if not matrix_a or not matrix_b:
         rows = 63
         cols = 426000
@@ -39,12 +29,10 @@ def test_matmult(matrix_a=None, matrix_b=None):
         matrix_b = cp.array(matrix_b)
 
     matrix_c = cp.matmul(matrix_a, matrix_b)
-
     return cp.asnumpy(matrix_c)
 
 
 def load_data_from_file(path, lags, convert_csv=False):
-
     m_list = []
     l_list = []
     if convert_csv:
@@ -58,43 +46,44 @@ def load_data_from_file(path, lags, convert_csv=False):
             array = np.load(file).astype('f')
             # set any float <1e-15 to 0 (for efficiency)
             np.around(array, decimals=15, out=array)
-            def subfunc(m):
-                return m.group(1)+m.group(2) if len(m.group(2)) == 2 else m.group(1)+"0"+m.group(2)
-            filename = re.sub(r'[a-z./]*([A-Za-z]+)([0-9]+)\.[a-z]+.', subfunc, file)
+
+            def renaming_function(m):
+                return m.group(1) + m.group(2) if len(m.group(2)) == 2 else m.group(1) + "0" + m.group(2)
+
+            filename = re.sub(r'[a-z./]*([A-Za-z]+)([0-9]+)\.[a-z]+.', renaming_function, file)
             # print(f"Array {file} has shape {np.shape(array)}")
             m_list.append(array)
             l_list.append(lags[filename])
+
     return m_list, l_list
 
 
 def test():
-    DEBUG = False
+    # Testing pattern
+    path_pattern = "./data/TS10*"
+    # Complete pattern
+    # path_pattern = "./data/*"
 
-    if DEBUG:
-        PATH_PATTERN = "./data/TS10*"
-    else:
-        PATH_PATTERN = "./data/*"
+    test_lags = {"TS01": 20,
+                 "TS02": 20,
+                 "TS03": 41,
+                 "TS04": 60,
+                 "TS05": 100,
+                 "TS06": 120,
+                 "TS07": 80,
+                 "TS08": 50,
+                 "TS09": 34,
+                 "TS10": 25
+                 }
 
-    LAGS = {"TS01": 20,
-            "TS02": 20,
-            "TS03": 41,
-            "TS04": 60,
-            "TS05": 100,
-            "TS06": 120,
-            "TS07": 80,
-            "TS08": 50,
-            "TS09": 34,
-            "TS10": 25
-            }
-
-    data_list = load_data_from_file(PATH_PATTERN, LAGS)
-    ret = do_correlation(*data_list)
+    data_list = load_data_from_file(path_pattern, test_lags)
+    ret = cross_correlation(*data_list)
     return [cp.asnumpy(r) for r in ret]
 
 
-def run(m_list, l_list):
-    m_list = [np.array(i) for i in m_list]
-    l_list = [int(i) for i in l_list]
+def matlab_xcorr(m_list, l_list):
+    m_list = [np.array(data) for data in m_list]
+    l_list = [int(lag) for lag in l_list]
     if len(l_list) != len(m_list):
         print("Data and lags have different sizes! Exiting...")
         return
@@ -104,7 +93,7 @@ def run(m_list, l_list):
                   f"setting lag value to {m_list[i].shape[1] - 1}")
             l_list[i] = m_list[i].shape[1] - 1
 
-    ret = do_correlation(m_list, l_list)
+    ret = cross_correlation(m_list, l_list)
     return [cp.asnumpy(r) for r in ret]
 
 
